d9b95a9200bcd51612258d23cc82f4d9180b1be6
Ben Shadid <bennxt1324@gmail.com>
2017-12-11 15:32:46 -0500

    slight modifications to HUD

--- frontyard-baseball-1.js
+++ frontyard-baseball-1.js
@@ -5,11 +5,12 @@ Physijs.scripts.ammo = 'js/ammo.js';
 
 var container;
 
-var camera, scene, renderer, loader, ball = null, materialBall, batBox, batBoxTexture;
+var camera, scene, renderer, loader, ball = null, materialBall, batBox, batBoxTexture, batTarget, batTargetTexture;
 
 var flag1 = false, flag2 = false, reset = true, startcheck;
 
 var bat, swinging, doneSwinging, downSwing;
+var dummyX, dummyY;
 
 window.onload = function init() {
 		swinging = false;
@@ -20,7 +21,7 @@ window.onload = function init() {
 		camera.position.set(0, 0, 24);
 
 		scene = new Physijs.Scene();
-		scene.setGravity(new THREE.Vector3(0, -30, 0));
+		scene.setGravity(new THREE.Vector3(0, -.8, 0));
 		scene.addEventListener('update', function(){
 			if (ball !== null) {
 				var old = ball.getLinearVelocity();
@@ -28,7 +29,7 @@ window.onload = function init() {
 
 			scene.simulate(undefined,1);}
 		} );
-	scene.simulate();
+		scene.simulate();
 
 		var ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
 		scene.add(ambientLight);
@@ -54,13 +55,20 @@ window.onload = function init() {
 		floorMesh.receiveShadow = true;
 		scene.add(floorMesh);
 
-		var wallMaterial = Physijs.createMaterial( new THREE.MeshLambertMaterial({map: loader.load( 'textures/fence.png'), transparent: true}), 0.5, 0.2);
-		wallMaterial.map.wrapS = THREE.RepeatWrapping;
-		wallMaterial.map.wrapT = THREE.RepeatWrapping;
-		wallMaterial.map.repeat.set( 10, 1 );
-		var wallMesh1 = new Physijs.BoxMesh( new THREE.BoxGeometry(80,10,0.1), wallMaterial, 0);
-		var wallMesh2 = new Physijs.BoxMesh( new THREE.BoxGeometry(60,10,10), wallMaterial, 0);
-		var wallMesh3 = new Physijs.BoxMesh( new THREE.BoxGeometry(60,10,12), wallMaterial, 0);
+		//MeshBasicMaterial({ color: 0x888888
+		// MeshLambertMaterial({map: loader.load( 'textures/fence.png'),
+
+		var wallMaterial = new Physijs.createMaterial( new THREE.MeshPhongMaterial({ color: 0x888888, transparent: false}), 0.5, 0.2);
+		//allMaterial.map.wrapS = THREE.RepeatWrapping;
+		//wallMaterial.map.wrapT = THREE.RepeatWrapping;
+		//wallMaterial.map.repeat.set( 10, 1 );
+		var wallMesh1 = new Physijs.BoxMesh( new THREE.BoxGeometry(80,10,1), wallMaterial, 0);
+		var wallMesh2 = new Physijs.BoxMesh( new THREE.BoxGeometry(60,10,1), wallMaterial, 0);
+		var wallMesh3 = new Physijs.BoxMesh( new THREE.BoxGeometry(60,10,1), wallMaterial, 0);
+
+		wallMesh1.receiveShadow = true;
+		wallMesh2.receiveShadow = true;
+		wallMesh3.receiveShadow = true;
 
 		wallMesh1.position.z -= 60;
 	wallMesh1.position.y -= 0.6;
@@ -78,14 +86,16 @@ window.onload = function init() {
 	wallMesh3.position.y -= 0.6;
 		scene.add(wallMesh3);
 
+		//MeshBasicMaterial({ color: 0x888888
 
 		var hudMaterial = new Physijs.createMaterial( new THREE.MeshLambertMaterial({map: loader.load( 'textures/hud.png'), transparent: true, opacity: 0.5}), 0.5, 0.2);
-		var hudMesh1 = new Physijs.BoxMesh( new THREE.BoxGeometry(1,1,0.5), hudMaterial, 0);
+		var hudMesh1 = new Physijs.BoxMesh( new THREE.BoxGeometry(1,1,0.01), hudMaterial, 0);
 		hudMesh1.addEventListener( 'collision', function(object) {
-			var force = new THREE.Vector3(Math.random(), 40, -60 * Math.abs(Math.random()) - 3);
+			console.log("applied to stirke Box");
+			var force = new THREE.Vector3(Math.random(), 0.0, -10 * Math.abs(Math.random()) - 0);
 			object.applyCentralImpulse( force );
 		});
-		hudMesh1.position.z = 21;
+		hudMesh1.position.z = 21.5;
 		scene.add(hudMesh1);
 
 
@@ -126,10 +136,11 @@ window.onload = function init() {
 
 		});
 
-		batBoxTexture = new Physijs.createMaterial( new THREE.MeshLambertMaterial({map: loader.load( 'textures/bbbat.jpg') }), 0.5, 0.2);
+
+		batBoxTexture = new Physijs.createMaterial( new THREE.MeshLambertMaterial({map: loader.load( 'textures/bbbat.jpg') , transparent: true, opacity: 0}), 0.5, 0.2);
 		batBoxTexture.map.wrapT = batBoxTexture.map.wrapT = THREE.RepeatWrapping;
 		batBoxTexture.map.repeat.set(1, 1);
-		batBox = new Physijs.BoxMesh( new THREE.BoxGeometry(0.18, 0.1, 1.5), batBoxTexture, 0);
+		batBox = new Physijs.BoxMesh( new THREE.BoxGeometry(0.18, 0.1, 0.01), batBoxTexture, 0);
 		batBox.position.x = -1;
 		batBox.position.y = 0.25;
 		batBox.position.z = 21;
@@ -138,7 +149,7 @@ window.onload = function init() {
 		batBox.__dirtyPosition = true;
 
 		batBox.addEventListener( 'collision', function(object) {
-			console.log(object);
+			console.log("literally how");
 			var force = new THREE.Vector3(0, 40, 60);
 			object.applyCentralImpulse( force );
 		});
@@ -146,6 +157,26 @@ window.onload = function init() {
 		scene.add(batBox);
 
 
+
+		batTargetTexture = new Physijs.createMaterial( new THREE.MeshLambertMaterial({map: loader.load( 'textures/target.png'), transparent: true, opacity: 0.5 }), 0.5, 0.2);
+		batTargetTexture.map.wrapT = batTargetTexture.map.wrapS = THREE.RepeatWrapping;
+		batTargetTexture.map.repeat.set(1, 1);
+		batTarget = new Physijs.BoxMesh( new THREE.BoxGeometry(1.1, 1.1, 0.5), batTargetTexture, 0);
+		batTarget.position.x = -1;
+		batTarget.position.y = 0.25;
+		batTarget.position.z = 20.95;
+		//batTarget.__dirtyPosition = true;
+
+		batTarget.addEventListener( 'collision', function(object) {
+			if (swinging) {
+				console.log("collision on target congrats on this seriously");
+				var force = new THREE.Vector3(Math.random()-0.5, 40.0/(Math.random()*10.0 + 2), -3 * Math.abs(Math.random()) - 20);
+				object.applyCentralImpulse( force );
+			}
+		});
+
+
+		scene.add(batTarget);
 
 		renderer = new THREE.WebGLRenderer({antialias:true});
 		renderer.setPixelRatio(window.devicePixelRatio);
@@ -224,7 +255,12 @@ function onMouseMove( event ) {
 				batBox.position.set(bat.position.x, bat.position.y, bat.position.z);
 				batBox.setLinearVelocity(new THREE.Vector3(0, 0, 0));
 				//console.log(bat.position.x + " " + bat.position.y);
-				scene.simulate();
+				//scene.simulate();
+
+				dummyX = event.clientX / window.innerWidth  * 4 - 2;
+				dummyY = -event.clientY / window.innerHeight * 2 + 1;
+				batTarget.position.set(event.clientX, event.clientY, 20.5);
+				batTarget.setLinearVelocity(new THREE.Vector3(0, 0, 0));
 		}
 }
 
@@ -273,11 +309,11 @@ function animate() {
 
 function addNewBall() {
 
-	if (ball.position.z == -5 && ball.getLinearVelocity().z >= 0){
-		ball.setLinearVelocity(new THREE.Vector3(0, 10, 40));
+	if (ball.position.z == -5 && ball.getLinearVelocity().z >= 0) {
+		ball.setLinearVelocity(new THREE.Vector3(0, 10.0/6.0, (40.0/6.0)));
 	}
 
-	if ( (ball.position.y < -250 && (ball.position.z > 0 || ball.position.z < -5)) || (Math.abs(ball.getLinearVelocity().z) < 2) && (ball.position.y < 1)) {
+	if ( (ball.position.y < -10 && (ball.position.z > 0 || ball.position.z < -5)) || (Math.abs(ball.getLinearVelocity().z) < 2) && (ball.position.y < 1)) {
 				//console.log(ball.position);
 				//ball.position.set(Math.random() * 2, 0.5, -5.0 + Math.random());
 				//ball.position.y = 0.5;
@@ -302,7 +338,7 @@ function addNewBall() {
 						});*/
 
 						scene.add( ball );
-						ball.setLinearVelocity(new THREE.Vector3(0, 10, 40));
+						ball.setLinearVelocity(new THREE.Vector3(0, 10.0/6.0, 40.0/6.0));
 						ball.castShadow = true;
 
 						flag2 = true;
@@ -310,12 +346,12 @@ function addNewBall() {
 						flag1 = true;
 					}
 		}
-		console.log(ball.position.z);
+		//console.log(ball.position.z);
 
 		if (ball.position.z < 0 && flag1){
 			if (bat.position.z < ball.position.z)
 			{
-				ball.setLinearVelocity(new THREE.Vector3(0, 10, 10));
+				ball.setLinearVelocity(new THREE.Vector3(0, 10.0/6.0, 10.0/6.0));
 			}
 
 			flag1 = false;
@@ -337,5 +373,6 @@ function render() {
 			camera.lookAt(scene.position);
 		}
 		batBox.position.set(bat.position.x, bat.position.y, bat.position.z);
+		batTarget.position.set(dummyX, dummyY, 20.5);
 		renderer.render(scene, camera);
 }
